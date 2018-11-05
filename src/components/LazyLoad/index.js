49350ff895616a0b5fe3569c5f76906d7bda3fc0
alexahdp <alexahdp@gmail.com>
2018-11-05 17:20:01 +0300

    Added important commit for lazyload

--- src/components/LazyLoad/index.js
+++ src/components/LazyLoad/index.js
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react';
 
 // React.lazy - для ленивой загрузки компонентов
 // React.Suspense - для заглушек
+// https://blog.logrocket.com/lazy-loading-components-in-react-16-6-6cea535c0b52
+// можно сделать lazy-load по страницам в роутере
 
 const LazyComponent = React.lazy(() => import('./OtherComponent'));
 
